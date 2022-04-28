3e67320f18fd771314027c3989b5ba07b7370326
Draor <robson19990501@gmail.com>
2022-04-27 23:22:54 -0500

    creando lista de campeones

---
+++ champs.py
@@ -0,0 +1,6 @@
+#Valor 1
+champs_1 = ['brand', 'caitlyn', 'camile', 'darius', 'ezreal', 'illaoi', 'jarvaniv', 'kassadin', 'nocturne', 'poppy', 'singed', 'twitch', 'ziggs']
+champs_2 = ['ashe', 'blitzcrank', 'corki', 'lulu', 'quinn', 'reksai', 'sejuani', 'swain', 'syndra', 'talon', 'warwick', 'zilean', 'zyra']
+champs_3 = ['chogath', 'ekko', 'gangplank', 'gnar', 'leona', 'lucian', 'malzahar', 'missfortune', 'morgana', 'senna', 'tryndamere', 'vex', 'zac']
+champs_4 = ['ahri', 'alistar', 'braum', 'draven', 'irelia', 'jhin', 'khazix', 'orinana', 'renata', 'seraphine', 'sivir', 'vi']
+champs_5 = ['galio', 'jayce', 'jinx', 'kaisa', 'silco', 'tahmkench', 'veigar', 'viktor', 'zeri']
---
+++ prob_champ.py
@@ -0,0 +1,7 @@
+
+class Prob_Champ():
+    
+    def __init__(self, champ, lvl):
+        self.champ = champ
+        self.lvl = lvl
+        
