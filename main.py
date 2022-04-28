bcb5006b39a5a1a220d2089dbd69e1247be1870c
Draor <robson19990501@gmail.com>
2022-04-28 03:07:00 -0500

    organizando por diccionarios.tuplas y listas tiers, lvl y probabilidades

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from .champs import tiers
+from .levels import lvls
+
+num_champ = {'tier_1' : 39,
+             'tier_2' : 26,
+             'tier_3' : 21,
+             'tier_4' : 13,
+             'tier_5' : 10
+             }
+
+
 
 
 if __name__=='__main__':
